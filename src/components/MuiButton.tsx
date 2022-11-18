08de02ee5fa7c25f5e4238d1f862ccc15506d19e
seaboyz <seaboyz@hotmail.com>
2022-11-18 04:22:40 -0500

    Add color pallete

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -3,10 +3,21 @@ import { Stack } from "@mui/system"
 
 export const MuiButton = () => {
   return (
-    <Stack spacing={2} direction={"row"} >
-      <Button variant="text" href="www.google.com">Text</Button>
-      <Button variant="contained">Text</Button>
-      <Button variant="outlined">Text</Button>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction={"row"} >
+        <Button variant="text" href="www.google.com">Text</Button>
+        <Button variant="contained">Text</Button>
+        <Button variant="outlined">Text</Button>
+      </Stack>
+
+      <Stack spacing={2} direction={"row"}>
+        <Button variant="contained" color="primary">Primary</Button>
+        <Button variant="contained" color="secondary">Secondary</Button>
+        <Button variant="contained" color="error">Error</Button>
+        <Button variant="contained" color="warning">Warning</Button>
+        <Button variant="contained" color="info">Info</Button>
+        <Button variant="contained" color="success">Success</Button>
+      </Stack>
     </Stack>
   )
-}
+} 
