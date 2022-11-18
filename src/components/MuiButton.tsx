f82008651d4a8b797d6271f083e8d7ac89b2173f
seaboyz <seaboyz@hotmail.com>
2022-11-18 04:15:57 -0500

    Add MuiButton

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@mui/material"
+import { Stack } from "@mui/system"
+
 export const MuiButton = () => {
   return (
-    <div>MuiButton</div>
+    <Stack spacing={2} direction={"row"} >
+      <Button variant="text" href="www.google.com">Text</Button>
+      <Button variant="contained">Text</Button>
+      <Button variant="outlined">Text</Button>
+    </Stack>
   )
 }
